97692af20b8435839d7d9f990d880accda981f81
dotnet111119 <57640157+dotnet111119@users.noreply.github.com>
2019-12-26 19:12:34 +0200

    Add files via upload

---
+++ EF_LINQ.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_DEMO1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (CompanyEntities entities = new CompanyEntities())
+            {
+                Console.WriteLine("Print All:");
+                var result = entities.EMPLOYEES.ToList();
+                result.ForEach(company => Console.WriteLine(JsonConvert.SerializeObject(company)));
+                Console.WriteLine("================================="); // QUERY SYNTAX
+                var res1 = from e in entities.EMPLOYEES
+                           where e.AGE > 20
+                           select new { e.NAME , e.AGE };
+                res1.ToList().ForEach(e => Console.WriteLine(JsonConvert.SerializeObject(e)));
+                Console.WriteLine("================================="); // QUERY SYNTAX
+                var res2 = (from e in entities.EMPLOYEES
+                           where e.AGE > 20
+                           select e).ToList().Count;
+                Console.WriteLine(res2);
+                Console.WriteLine("================================="); // METHOD SYNTAX
+                // bool func(employee e)
+                // { 
+                //   return  e.Age > 20;
+                // }
+                // e => e.Age > 20;               
+                var res3 = entities.EMPLOYEES.ToList().Where(e => e.AGE > 20);
+                res1.ToList().ForEach(e => Console.WriteLine(JsonConvert.SerializeObject(e)));
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
